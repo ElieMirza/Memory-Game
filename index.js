a3d41bc3bf68a42285eaab6e4516853a28d7bd2f
ElieMirza <elie.mirza1987@gmail.com>
2022-01-27 11:21:25 +0200

    added further functionality to start game properly

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ var level = 0;
 var game_started = false;
 
 $(document).keypress(function() {
-  if (game_started == true) {
-    $("#title").text("Level " + level)
+  if (game_started == false) {
+    $("#title").text("Level " + level);
+    addToPattern();
+    game_started = true;
   }
 });
 
 function addToPattern() {
   var random_color = button_colors[Math.floor(Math.random() * 4)];
   generated_pattern.push(random_color);
-}
+};
 
 function playSound(mp3_name) {
   var audio = new Audio("sounds/" + mp3_name + ".mp3");
   audio.play();
-}
+};
