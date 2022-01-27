aa1764b02dfbb8724d86668d4525f824b2ebfbfc
ElieMirza <elie.mirza1987@gmail.com>
2022-01-27 12:13:44 +0200

    added play sound and updated pattern

--- index.js
+++ index.js
@@ -7,15 +7,23 @@ var game_started = false;
 
 $(document).keypress(function() {
   if (game_started == false) {
+    game_started = true;
     $("#title").text("Level " + level);
     addToPattern();
-    game_started = true;
   }
+});
+
+$(".btn").click(function() {
+  var player_clicked_color = $(this).attr("id");
+  player_pattern.push(player_clicked_color);
+  playSound(player_clicked_color);
 });
 
 function addToPattern() {
   var random_color = button_colors[Math.floor(Math.random() * 4)];
   generated_pattern.push(random_color);
+  $("#" + random_color).fadeIn(100).fadeOut(100). fadeIn(100);
+  playSound(random_color);
 };
 
 function playSound(mp3_name) {
