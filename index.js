dc91cce5289c06fd2ba2e2e246474dfd5cd2de0b
ElieMirza <elie.mirza1987@gmail.com>
2022-01-27 12:16:58 +0200

    updated addtopattern function

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ $(".btn").click(function() {
 });
 
 function addToPattern() {
+  player_pattern = [];
+  level++;
   var random_color = button_colors[Math.floor(Math.random() * 4)];
   generated_pattern.push(random_color);
   $("#" + random_color).fadeIn(100).fadeOut(100). fadeIn(100);
