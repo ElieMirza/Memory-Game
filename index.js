fb45a3163da9d9122184bd205acf69c11a289879
ElieMirza <elie.mirza1987@gmail.com>
2022-01-27 13:48:22 +0200

    cleaned code and added comments

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var generated_pattern = [];
 var player_pattern = [];
 var level = 0;
 
-// Checks if game is started
+// Checks if game is started on keypress
+// so the game doesn't restart at every keypress
 $(document).keypress(function() {
   if (game_started == false) {
     game_started = true;
@@ -14,6 +15,8 @@ $(document).keypress(function() {
   }
 });
 
+// Listens for btn clicks from player, plays animation and sound,
+// then check if clicked color is correct or wrong
 $(".btn").click(function() {
   var player_clicked_color = $(this).attr("id");
   player_pattern.push(player_clicked_color);
@@ -22,28 +25,9 @@ $(".btn").click(function() {
   checkStatus(player_pattern.length - 1);
 });
 
-function addToPattern() {
-  player_pattern = [];
-  level++;
-  $("#title").text("Level: " + level);
-  var random_color = button_colors[Math.floor(Math.random() * 4)];
-  generated_pattern.push(random_color);
-  $("#" + random_color).fadeIn(100).fadeOut(100). fadeIn(100);
-  playSound(random_color);
-};
-
-function playSound(mp3_name) {
-  var audio = new Audio("sounds/" + mp3_name + ".mp3");
-  audio.play();
-};
-
-function animateButton(clicked_color) {
-  $("#" + clicked_color).addClass("pressed");
-  setTimeout(function() {
-    $("#" + clicked_color).removeClass("pressed");
-  }, 100);
-};
-
+// Compares player patter with generated addToPattern
+// If completed, it adds another pattern after 1 second
+// If not, game over, reset game
 function checkStatus(lvl) {
   if (generated_pattern[lvl] === player_pattern[lvl]) {
     if (player_pattern.length === generated_pattern.length) {
@@ -63,6 +47,34 @@ function checkStatus(lvl) {
   }
 }
 
+// resets player previously clicked buttons, increments level and updates text
+// chooses random color and appends it to generated_pattern
+// animates chosen button and plays relevant sound
+function addToPattern() {
+  player_pattern = [];
+  level++;
+  $("#title").text("Level: " + level);
+  var random_color = button_colors[Math.floor(Math.random() * 4)];
+  generated_pattern.push(random_color);
+  $("#" + random_color).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(random_color);
+};
+
+// Plays sounds
+function playSound(mp3_name) {
+  var audio = new Audio("sounds/" + mp3_name + ".mp3");
+  audio.play();
+};
+
+// Animates buttons by appying css class then removing it
+function animateButton(clicked_color) {
+  $("#" + clicked_color).addClass("pressed");
+  setTimeout(function() {
+    $("#" + clicked_color).removeClass("pressed");
+  }, 100);
+};
+
+// This function resets the game
 function resetGame() {
   game_started = false;
   level = 0;
