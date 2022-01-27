341834e276708912cf6268d1163544401971c4c0
ElieMirza <elie.mirza1987@gmail.com>
2022-01-27 11:13:05 +0200

    added function to play sound

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function addToPattern() {
   var random_color = button_colors[Math.floor(Math.random() * 4)];
   generated_pattern.push(random_color);
 }
+
+function playSound(mp3_name) {
+  var audio = new Audio("sounds/" + mp3_name + ".mp3");
+  audio.play();
+}
