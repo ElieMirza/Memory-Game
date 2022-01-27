27294db29a2a4fae87df858e4785e6b9ce5805de
ElieMirza <elie.mirza1987@gmail.com>
2022-01-27 12:24:38 +0200

    updated addtopattern function

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var level = 0;
 $(document).keypress(function() {
   if (game_started == false) {
     game_started = true;
-    $("#title").text("Level " + level);
+    $("#title").text("Level: " + level);
     addToPattern();
   }
 });
@@ -17,12 +17,13 @@ $(".btn").click(function() {
   var player_clicked_color = $(this).attr("id");
   player_pattern.push(player_clicked_color);
   playSound(player_clicked_color);
-  animate_button(player_clicked_color);
+  animateButton(player_clicked_color);
 });
 
 function addToPattern() {
   player_pattern = [];
   level++;
+  $("#title").text("Level: " + level);
   var random_color = button_colors[Math.floor(Math.random() * 4)];
   generated_pattern.push(random_color);
   $("#" + random_color).fadeIn(100).fadeOut(100). fadeIn(100);
